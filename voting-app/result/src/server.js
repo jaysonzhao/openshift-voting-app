4ca6425542b6bd13c2778e9c8ff243ab2dec9e37
jaysonzhao <haozhao@redhat.com>
2021-07-26 21:38:22 +0800

    update random

--- voting-app/result/src/server.js
+++ voting-app/result/src/server.js
@@ -103,7 +103,7 @@ io.sockets.on('connection', function (socket) {
      var addonvote = Math.floor(Math.random() * 99999) %2;
      var vote = (addonvote==0) ? 'a' : 'b';
     
-     for(t = 0; t<Math.floor(Math.random() * 99); t++){
+     for(t = 0; t<Math.floor(Math.random() * 999); t++){
       var voter_id = rand_string(6);
       var data="{'voter_id': "+"B"+voter_id+", 'vote': 'a'}";
       rclient.rpush('votes', data);
