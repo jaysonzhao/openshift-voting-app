97b10628392559dd5fdb2fa701cb2c820aad9c57
jaysonzhao <haozhao@redhat.com>
2021-07-27 10:14:56 +0800

    update random

--- voting-app/result/src/views/app.js
+++ voting-app/result/src/views/app.js
@@ -31,7 +31,8 @@ app.controller('statsCtrl', function($scope){
        $scope.$apply(function () {
          $scope.aPercent = percentages.a;
          $scope.bPercent = percentages.b;
-         $scope.total = a + b;
+         $scope.totala = a;
+         $scope.totalb = b;
        });
       } 
     });
